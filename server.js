9b0886041a65c99c6d6c31eb0b7eb14bdfc42311
André Felicio <andrefelicio@sisconsultoria.com.br>
2023-03-13 18:23:01 -0300

    Configração server.js

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
-//importar express
 const express = require('express');
-//iniciar express
-const app = express();
-//nome da pasta no dist que será feito o build
+const path = require('path');
 const appName = 'toca-fominha-front';
-//local onde o build irá gerar os arquivos
-const outputPath = `${__dirname}/dist/${appName};`
+const app = express();
 
-//setar o diretório de build para servir o conteudo Angular
-app.use(express.static(outputPath));
-//redirecionar qualquer requisição para o index.html
-// app.get('/*', (req, res) => {
-//     res.sendFile(`${outputPath}/index.html`);
-// });
+app.use(express.static(`${__dirname}/dist/${appName}`));
+
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname,'dist',appName,'index.html'));
+    res.sendFile(path.join(`${__dirname}/dist/${appName}/index.html`));
 });
+
 //ouvir a porta que o Heroku disponibilizar
 app.listen(process.env.PORT);
