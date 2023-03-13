35fe8fcdfd7e4e0ae9bf709d6e54af6d55751a27
André Felicio <andrefelicio@sisconsultoria.com.br>
2023-03-13 17:18:31 -0300

    Ajusta arquivo server.js

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ const outputPath = `${__dirname}/dist/${appName};`
 //setar o diretório de build para servir o conteudo Angular
 app.use(express.static(outputPath));
 //redirecionar qualquer requisição para o index.html
+// app.get('/*', (req, res) => {
+//     res.sendFile(`${outputPath}/index.html`);
+// });
 app.get('/*', (req, res) => {
-    res.sendFile(`${outputPath}/index.html`);
+    res.sendFile(path.join(__dirname,'dist',appName,'index.html'));
 });
 //ouvir a porta que o Heroku disponibilizar
 app.listen(process.env.PORT);
