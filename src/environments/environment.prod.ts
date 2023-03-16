f0b15a432b518679152ba3505ed2c59645fb0dc6
André Felicio <andrefelicio@sisconsultoria.com.br>
2023-03-16 16:37:03 -0300

    Ajusta variavel de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,21 +1,21 @@
 export const environment = {
   production: true,
-  firebaseConfig: {//***DESCONMENTAR ESSE OBJETO ABAIXO QUANDO ESTIVER NA BRANCH RELEASE/ESTEIRA E DESCOMENTAR O OBJETO ABAIXO DESSE */
-    apiKey: 'AIzaSyAuykiSP5JWTFGvxajRuhXNkg47bMthZqE',
-    authDomain: 'toca-fominha.firebaseapp.com',
-    projectId: 'toca-fominha',
-    storageBucket: 'toca-fominha.appspot.com',
-    messagingSenderId: '100452196370',
-    appId: '1:100452196370:web:9d2dd6c584ac53738cb8cb',
-  },
+  // firebaseConfig: {//***DESCONMENTAR ESSE OBJETO ABAIXO QUANDO ESTIVER NA BRANCH RELEASE/ESTEIRA E DESCOMENTAR O OBJETO ABAIXO DESSE */
+  //   apiKey: 'AIzaSyAuykiSP5JWTFGvxajRuhXNkg47bMthZqE',
+  //   authDomain: 'toca-fominha.firebaseapp.com',
+  //   projectId: 'toca-fominha',
+  //   storageBucket: 'toca-fominha.appspot.com',
+  //   messagingSenderId: '100452196370',
+  //   appId: '1:100452196370:web:9d2dd6c584ac53738cb8cb',
+  // },
 
-  // firebaseConfig: { //***DESCONMENTAR ESSE OBJETO ABAIXO QUANDO ESTIVER NA BRANCH RELEASE/DEV E COMENTAR O OBJETO ACIMA */
-  //   apiKey: 'AIzaSyALnvoK_6yQhkUQAydqFVenPc5nfbx-Rt0',
-  //   authDomain: 'toca-fominha-dev.firebaseapp.com',
-  //   projectId: 'toca-fominha-dev',
-  //   storageBucket: 'toca-fominha-dev.appspot.com',
-  //   messagingSenderId: '1058881912976',
-  //   appId: '1:1058881912976:web:bc7c7a321699d199929815',
-  //   measurementId: 'G-E23NL8YSJM',
-  // }
+  firebaseConfig: { //***DESCONMENTAR ESSE OBJETO ABAIXO QUANDO ESTIVER NA BRANCH RELEASE/DEV E COMENTAR O OBJETO ACIMA */
+    apiKey: 'AIzaSyALnvoK_6yQhkUQAydqFVenPc5nfbx-Rt0',
+    authDomain: 'toca-fominha-dev.firebaseapp.com',
+    projectId: 'toca-fominha-dev',
+    storageBucket: 'toca-fominha-dev.appspot.com',
+    messagingSenderId: '1058881912976',
+    appId: '1:1058881912976:web:bc7c7a321699d199929815',
+    measurementId: 'G-E23NL8YSJM',
+  }
 };
