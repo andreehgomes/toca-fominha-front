176b6113830c50ae6d6cfa265116479d1fe2437a
André Felicio <andrefelicio@sisconsultoria.com.br>
2023-03-15 21:30:15 -0300

    Ajusta config firebase para pegar as variaveis de ambiente quando tiver no ambiente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,30 @@ import config from '../assets/config.json';
 
 registerLocaleData(localePt, 'pt');
 
+export const getConfigFireModule = () => {
+  if (environment.production) {
+    return {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+      measurementId: process.env.measurementId,
+    };
+  } else {
+    return {
+      apiKey: 'AIzaSyALnvoK_6yQhkUQAydqFVenPc5nfbx-Rt0',
+      authDomain: 'toca-fominha-dev.firebaseapp.com',
+      projectId: 'toca-fominha-dev',
+      storageBucket: 'toca-fominha-dev.appspot.com',
+      messagingSenderId: '1058881912976',
+      appId: '1:1058881912976:web:bc7c7a321699d199929815',
+      measurementId: 'G-E23NL8YSJM',
+    };
+  }
+};
+
 @NgModule({
   declarations: [AppComponent, LoaderComponent, PageErrorComponent],
   imports: [
@@ -32,15 +56,7 @@ registerLocaleData(localePt, 'pt');
     MaterialModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp({
-      apiKey: 'AIzaSyALnvoK_6yQhkUQAydqFVenPc5nfbx-Rt0',
-      authDomain: 'toca-fominha-dev.firebaseapp.com',
-      projectId: 'toca-fominha-dev',
-      storageBucket: 'toca-fominha-dev.appspot.com',
-      messagingSenderId: '1058881912976',
-      appId: '1:1058881912976:web:bc7c7a321699d199929815',
-      measurementId: 'G-E23NL8YSJM',
-    }),
+    AngularFireModule.initializeApp(getConfigFireModule),
     AngularFirestoreModule,
     AngularFireDatabaseModule,
   ],
