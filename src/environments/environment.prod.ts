facb76b39f26c85fcd449f6a68627b9f3c8f3648
André Felicio <andrefelicio@sisconsultoria.com.br>
2023-03-16 08:24:58 -0300

    Ajuste enviroment .prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,21 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAuykiSP5JWTFGvxajRuhXNkg47bMthZqE",
-    authDomain: "toca-fominha.firebaseapp.com",
-    projectId: "toca-fominha",
-    storageBucket: "toca-fominha.appspot.com",
-    messagingSenderId: "100452196370",
-    appId: "1:100452196370:web:9d2dd6c584ac53738cb8cb"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   }
 };
+
+// firebaseConfig: {
+//   apiKey: "AIzaSyAuykiSP5JWTFGvxajRuhXNkg47bMthZqE",
+//   authDomain: "toca-fominha.firebaseapp.com",
+//   projectId: "toca-fominha",
+//   storageBucket: "toca-fominha.appspot.com",
+//   messagingSenderId: "100452196370",
+//   appId: "1:100452196370:web:9d2dd6c584ac53738cb8cb"
+// }
